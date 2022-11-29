be422df7ed30c39e459f6f9e8b62be2dc5d6c74a
Yalalt <yalaltgandush@gmail.com>
2022-11-29 18:45:16 +0800

    max Number oloh js bichiw

--- js/basic/condition/maxNumber/main.js
+++ js/basic/condition/maxNumber/main.js
@@ -1,23 +1,52 @@
 console.log("------ a B c toonii haritsuullalt --------");
-let x = 10, y = 28, z = 33;
+let x = Math.floor(Math.random() * 77) + 1;
+let y = Math.floor(Math.random() * 77) + 1;
+let z = Math.floor(Math.random() * 77) + 1;
+
+console.log("X: " + x);
+console.log("Y: " + y);
+console.log("Z: " + z);
+
+let hamgiinUxUtga = x;
+let hamgiinBagaUtga = y;
 
 if(x > y) {
     if (x > z) {
-        console.log(x, " - X too ni hamgiin ih");
+        hamgiinUxUtga = x + " - X too ni hamgiin ih";
     } else {
-        console.log(z, " - Z too ni hamgiin ih");
+        hamgiinUxUtga = z + " - Z too ni hamgiin ih";
     }
 } else if(y > z) {
-    console.log(y, " - Y too ni hamgiin ih");
+    hamgiinUxUtga = y + " - Y too ni hamgiin ih";
 } else {
-    console.log(z, " - Z too ni hamgiin ih");
+    hamgiinUxUtga = z + " - Z too ni hamgiin ih";
 }
 
+// if
+// x y z
+// z x y
+// x z y
+// else
+// z y x
+// y z x
+// y x z
 if(x > y){
     if(z > y){
-        console.log(y, " - Y too ni hamgiin baga");
-    } 
+        hamgiinBagaUtga = y + " - Y too ni hamgiin baga";
+    } else {
+        hamgiinBagaUtga = z + " - Z too ni hamgiin baga";
+    }
+} else if(y > z) {
+    if(z > x) {
+        hamgiinBagaUtga = x + " - X too ni hamgiin baga";
+    } else {
+        hamgiinBagaUtga = z + " - Z too ni hamgiin baga";
+    }
+} else {
+    hamgiinBagaUtga = x + " - X too ni hamgiin baga";
 }
 
+console.log(hamgiinUxUtga);
+console.log(hamgiinBagaUtga);
 console.log("-");
 console.log("-");
