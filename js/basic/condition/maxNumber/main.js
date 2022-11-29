b14dc4a8e0c2c61fb1e6078100cf06ae03b53506
Yalalt <yalaltgandush@gmail.com>
2022-11-29 12:44:10 +0800

    Hamgiin baga toonuud oloh

---
+++ js/basic/condition/maxNumber/main.js
@@ -0,0 +1,23 @@
+console.log("------ a B c toonii haritsuullalt --------");
+let x = 10, y = 28, z = 33;
+
+if(x > y) {
+    if (x > z) {
+        console.log(x, " - X too ni hamgiin ih");
+    } else {
+        console.log(z, " - Z too ni hamgiin ih");
+    }
+} else if(y > z) {
+    console.log(y, " - Y too ni hamgiin ih");
+} else {
+    console.log(z, " - Z too ni hamgiin ih");
+}
+
+if(x > y){
+    if(z > y){
+        console.log(y, " - Y too ni hamgiin baga");
+    } 
+}
+
+console.log("-");
+console.log("-");
