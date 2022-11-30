dd012c6639640868469730d0a220dea77087bef6
Yalalt <yalaltgandush@gmail.com>
2022-11-30 11:49:03 +0800

    rename hiiw

--- js/basic/condition/randomNumbers/main.js
+++ js/basic/condition/randomNumbers/main.js
@@ -1,8 +1,8 @@
 // 2 too prompt
 console.log("1. 2 too prompt awna");
 
-let ehlelToo = Number(prompt("Ehnii toog oruulna uu"));
-let daraagiinToo = Number(prompt("Daraagiin toog oruul"));
+let startoo = Number(prompt("Ehnii toog oruulna uu"));
+let endtoo = Number(prompt("Daraagiin toog oruul"));
 
-let generatedToo = Math.floor((Math.random() * daraagiinToo) + ehlelToo);
+let generatedToo = Math.floor((Math.random() * endtoo) + startoo);
 console.log(generatedToo);
